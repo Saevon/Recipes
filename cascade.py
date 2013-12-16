d147f1263d57185c20d8b8c716ae0d06b7e3e240
Serghei Filippov <blastowind@gmail.com>
2013-12-16 16:52:22 -0500

    Adds a chain/cascade decorator for methods.

---
+++ cascade.py
@@ -0,0 +1,12 @@
+from functools import wraps
+
+
+def cascade(func):
+	'''
+	A method wrapper that ensures that self is always returned
+	'''
+	@wraps(func)
+	def wrapper(self, *args, **kwargs):
+		func(self, *args, **kwargs)
+		return self
+	return wrapper
