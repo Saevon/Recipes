be771ded430292a03fa6bde040f4b0718a829d36
Serghei Filippov <blastowind@gmail.com>
2015-05-29 20:43:20 -0400

    Sorts bash command notes

--- cmd.bash
+++ cmd.bash
@@ -1,32 +1,70 @@
+##########################
+# Mac
+##########################
 
-# Reading Japanese Files
-iconv -f SHIFT_JIS -t utf-8 "Read me.txt" > UTF8-Readme.txt 
-# Also, try opening file in chrome, and view >> encodings...
-
-
-# Python env Fix
-export ARCHFLAGS="-Wno-error=unused-command-line-argument-hard-error-in-future"
-
-# Get space of folders
-du -hs .[!.]* *
-
-# Hide File (Mac)
+# Hide File
 sudo chflags hidden $FILE
 
 # Fix Duplicate “Open With”
 /System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/\LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user
 
+# Python env Fix
+export ARCHFLAGS="-Wno-error=unused-command-line-argument-hard-error-in-future"
+
 # Mac Incrementl Adjust: Opt-Shift (vol, bright)
+
+
+
+
+
+###########################################
+# Misc
+##########################################
+
+# Get space of folders
+du -hs .[!.]* *
 
 # Terminal notification (number)
 tput bel
 
-# Makes a job a daemon (won't quit on terminal exit)
-disown -h
-
 # Add commands to running process
 #<Ctrl-Z>: aka pause the job
 fg && tput bel
+
+# Reading Japanese Files
+iconv -f SHIFT_JIS -t utf-8 "Read me.txt" > UTF8-Readme.txt
+# Also, try opening file in chrome, and view >> encodings...
+
+
+# Makes a job a daemon (won't quit on terminal exit)
+disown -h
+
+
+# XARGS: command2 will get the entire output as args (replacing \n with space)
+command | xargs command2
+# XARGS: for files with spaces (replace show_args.py with your
+find . -name "*.mp3" -print0 | xargs -I{} -0 ~/Projects/Recipe/show_args.py {}
+
+
+# See all open ports on machine
+# without sudo this only shows your processes
+sudo lsof -PiTCP -sTCP:LISTEN
+
+
+
+
+#################################################
+# Bash Syntax
+#################################################
+
+# If statements
+if [ $? -ne 0 ]; then
+    ...
+fi
+# equivalent to:
+if command; then : ; else
+    ...
+fi
 
 
 # Bash file reading, if you use backticks, you might reach the ARG_MAX
@@ -39,20 +77,6 @@ command |
 while read f; do
     ...
 done
-# XARGS: command2 will get the entire output as args (replacing \n with space)
-command | xargs command2
-# XARGS: for files with spaces (replace show_args.py with your
-find . -name "*.mp3" -print0 | xargs -I{} -0 ~/Projects/Recipe/show_args.py {}
-
-
-# If statements
-if [ $? -ne 0 ]; then
-    ...
-fi
-# equivalent to:
-if command; then : ; else
-    ...
-fi
 
 # SIGNALS:
 # SIGINT: Ctrl-C
@@ -61,9 +85,3 @@ fi
 # SIGHUP: terminal death (hangup), use nohup to cause a thing not to exit on this
 # SIGINT: Immediate kill
 # Proper kill (give each some time): TERM, INT, UP, KILL
-
-
-
-# See all open ports on machine
-# without sudo this only shows your processes
-sudo lsof -PiTCP -sTCP:LISTEN
