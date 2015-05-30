0d6414f4bea5dc6ecad2bb89c4f7920d3b2edaad
Serghei Filippov <blastowind@gmail.com>
2015-05-29 20:25:30 -0400

    Minor cleanup

--- cascade.py
+++ cascade.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 from functools import wraps
 
 
 def cascade(func):
-	'''
-	A method wrapper that ensures that self is always returned
-	'''
-	@wraps(func)
-	def wrapper(self, *args, **kwargs):
-		func(self, *args, **kwargs)
-		return self
-	return wrapper
+    '''
+    A method wrapper that ensures that self is always returned
+    '''
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        func(self, *args, **kwargs)
+        return self
+    return wrapper
--- string_helpers.py
+++ string_helpers.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+
 def capitalize_words(string):
     '''
     Capitalizes every word in the string
     '''
     ' ' .join([s.capitalize() for s in string.split(' ')])
+
