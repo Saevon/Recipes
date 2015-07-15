d6e201429f2838ce8f15811cf36edf55c1aa5c3e
Serghei Filippov <blastowind@gmail.com>
2015-07-14 21:04:15 -0400

    Adds more helpful commands

--- cmd.bash
+++ cmd.bash
@@ -15,6 +15,37 @@ export ARCHFLAGS="-Wno-error=unused-command-line-argument-hard-error-in-future"
 
 
 
+# Set default permissions under a directory
+# MAC ONLY: Unix uses setfacl (see unix section below)
+
+# Read ACL
+ls -le
+# Write ACL
+chmod +a 'group allow <permissions>' <directory>
+chmod +a 'group deny <permissions>' <directory>
+chown Saevon:self <directory>
+chmod u=rwX,g=rwX,o= <directory>
+# Clear ACL
+chmod -N <directory>
+
+# Working with Extended Attributes (the @)
+ls -l@
+# clear all
+xattr -c
+# delete one
+xattr -d <attribute>
+
+
+
+# Fast user Switching
+# to Login Window
+/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
+#Specific User
+/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -switchToUserID `id -u $USERNAME`
+
+
+# Restart Audio Service, get the pid
+ps aux | grep coreaudio
 
 
 ###########################################
@@ -51,6 +82,28 @@ find . -name "*.mp3" -print0 | xargs -I{} -0 ~/Projects/Recipe/show_args.py {}
 sudo lsof -PiTCP -sTCP:LISTEN
 
 
+# Set default permissions under a directory
+# UNIX ONLY: Mac uses chmod (see mac section above)
+setfacl <directory>
+getfacl <directory>
+
+
+# CronTab times
+# *     ~ all values
+# 1,4   ~ 1 and 4
+# 3,9/2 ~ from 3 to 9 with a step of 2 (aka 3,5,7,9)
+# */3   ~ all values with a step of 3
+# 
+# Special values are also allowed
+# @reboot
+# @yearly/@annually
+# @monthly
+# @weekly
+# @daily/@midnight
+# @hourly
+
+# generate private key from public key
+ssh-keygen -y -f id_rsa -C "serghei@Windy" > id_rsa.pub
 
 
 #################################################
